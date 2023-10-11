0b555ba5b4238197dc8e4f84e8fefcb128c250c3
KevzPeter <ultimatekevin1@gmail.com>
2023-10-11 12:58:36 +0530

    ✨ Randomized Icons!

--- components/SvgWidget.tsx
+++ components/SvgWidget.tsx
@@ -1,9 +1,14 @@
 // @ts-nocheck
 import Duo_Waving from "../public/svg/Duo_waving.svg";
 import Duo_PencilBoarding from "../public/svg/Duo_PencilBoarding.svg";
+import Duo_Excited from "../public/svg/Duo_Excited.svg";
+import Duo_Jolly from "../public/svg/Duo_Jolly.svg";
+import Duo_Amour from "../public/svg/Duo_Amour.svg";
+import Duo_Cannonball from "../public/svg/Duo_Cannonball.svg";
+import Duo_Daunting from "../public/svg/Duo_Daunting.svg";
 import Crown from "../public/svg/crown.svg";
 import Streak from "../public/svg/streak.svg";
-import Duo from "../public/svg/logo_0037_duo.svg"
+import Duo from "../public/svg/logo_0037_duo.svg";
 import En from "../public/svg/logo_0000_en.svg";
 import Es from "../public/svg/logo_0001_es.svg";
 import Fr from "../public/svg/logo_0002_fr.svg";
@@ -33,68 +38,89 @@ import Ro from "../public/svg/logo_0025_ro.svg";
 import Sw from "../public/svg/logo_0026_sw.svg";
 import Eo from "../public/svg/logo_0027_eo.svg";
 import World from "../public/svg/logo_0036_world.svg";
-import themes from '../utils/themes.json';
+import themes from "../utils/themes.json";
 /**
  * The main SVG widget.
  */
-export default function SvgWidget({response, theme}): JSX.Element {
+export default function SvgWidget({ response, theme }): JSX.Element {
     const flagComponents = {
-        "duo" : Duo,
-        "en" : En ,
-        "es": Es ,
-        "fr" : Fr ,
-        "de" : De ,
-        "hi" : Hi ,
-        "ja" : Ja ,
-        "it" : It ,
-        "ko" : Ko ,
-        "zh" : Zh ,
-        "pt" : Pt ,
-        "ru" : Ru ,
-        "tr" : Tr ,
-        "nl" : Nl ,
-        "sv" : Sv ,
-        "el" : El ,
-        "iw" : Iw ,
-        "pl" : Pl ,
-        "no" : No ,
-        "vi" : Vi ,
-        "da" : Da ,
-        "ar" : Ar ,
-        "uk" : Uk ,
-        "ga" : Ga ,
-        "la" : La ,
-        "hv" : Hv ,
-        "ro" : Ro ,
-        "sw" : Sw ,
-        "eo" : Eo ,
-        "world" : World,
-       } 
+        duo: Duo,
+        en: En,
+        es: Es,
+        fr: Fr,
+        de: De,
+        hi: Hi,
+        ja: Ja,
+        it: It,
+        ko: Ko,
+        zh: Zh,
+        pt: Pt,
+        ru: Ru,
+        tr: Tr,
+        nl: Nl,
+        sv: Sv,
+        el: El,
+        iw: Iw,
+        pl: Pl,
+        no: No,
+        vi: Vi,
+        da: Da,
+        ar: Ar,
+        uk: Uk,
+        ga: Ga,
+        la: La,
+        hv: Hv,
+        ro: Ro,
+        sw: Sw,
+        eo: Eo,
+        world: World,
+    };
+
+    const duoIcons = [
+        { icon: Duo_Waving, viewBox: "0 0 281 349" },
+        { icon: Duo_PencilBoarding, viewBox: "40 50 225 200" },
+        { icon: Duo_Jolly, viewBox: "0 0 185 180", style: 'flip' },
+        { icon: Duo_Excited, viewBox: "0 0 160 210", style: 'flip' },
+        { icon: Duo_Daunting, viewBox: "0 0 190 150" },
+        { icon: Duo_Cannonball, viewBox: "0 0 160 150" },
+        { icon: Duo_Amour, viewBox: "0 0 185 30", style: 'flip' },
+    ];
+
+    const randomIndex = Math.floor(Math.random() * 7);
+    const DuoIconComponent = duoIcons[randomIndex].icon;
+    const viewBox = duoIcons[randomIndex].viewBox;
+    const iconStyle = duoIcons[randomIndex].style;
+
     return (
         <g>
             <foreignObject x="0" y="0" width="100%" height="100%">
                 <div xmlns="http://www.w3.org/1999/xhtml">
-                        <div className="card" style={{'background-color': `${themes[theme].background}`, 'color': `${themes[theme].colorPrimary}`}}>
-                            <div className='content'>
-                                <div className='stats'>
-                                    <span id="streak"><Streak height={40} width={40}  viewBox="0 0 250 283"/>{response.streak} Day streak</span>
-                                    <span id="xp">⚡{response.totalXp} XP</span>
-                                </div>
-                                <div className='courses' style={{'color': `${themes[theme].colorSecondary}`}}>
-                                    {response.courses.slice(0, Math.min(3, response.courses.length)).map((course, idx) => {
-                                        const FlagComponent = flagComponents[course.learningLanguage] || flagComponents.world;
-                                        return(
-                                        <div className='language' key={idx}>
-                                            <Crown width={17} height={13} viewBox="0 0 353 268"/>
-                                            <span id='crown-count'>{course.crowns}</span>
-                                            <FlagComponent height={37} width={50} viewBox="0 0 78 62"/>
-                                        </div>
-                                    )})}
-                                </div>
+                    <div className="card" style={{ "background-color": `${themes[theme].background}`, color: `${themes[theme].colorPrimary}` }}>
+                        <div className="content">
+                            <div className="stats">
+                                <span id="streak">
+                                    <Streak height={40} width={40} viewBox="0 0 250 283" />
+                                    {response.streak} Day streak
+                                </span>
+                                <span id="xp">⚡{response.totalXp} XP</span>
                             </div>
-                            <Duo_Waving height={152} width={122}  viewBox="0 0 281 349"/>
-                            {/* <Duo_PencilBoarding height={155} width={155}  viewBox="0 0 300 300"/> */}
+                            <div className="courses" style={{ color: `${themes[theme].colorSecondary}` }}>
+                                {response.courses.slice(0, Math.min(3, response.courses.length)).map((course, idx) => {
+                                    const FlagComponent = flagComponents[course.learningLanguage] || flagComponents.world;
+                                    return (
+                                        <div className="language" key={idx}>
+                                            <Crown width={17} height={13} viewBox="0 0 353 268" />
+                                            <span id="crown-count">{course.crowns}</span>
+                                            <FlagComponent height={37} width={50} viewBox="0 0 78 62" />
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
+                        {iconStyle === 'flip' ?
+                            <DuoIconComponent height={152} width={122} viewBox={viewBox} style={{ 'display': 'block', 'transform': 'scale(-1, 1)' }} /> :
+                            <DuoIconComponent height={152} width={122} viewBox={viewBox} />}
+                    </div>
                 </div>
             </foreignObject>
         </g>
