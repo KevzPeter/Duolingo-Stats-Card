6620df7ee046aa898840ab7f48899da69fba648f
KevzPeter <ultimatekevin1@gmail.com>
2025-09-21 21:40:07 -0400

    📈 Added Vercel Analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.scss'
 import { CSSProperties } from "react";
 import { THEME_NAMES } from '../utils/config';
+import { Analytics } from '@vercel/analytics/next';
 
 const Home: NextPage = () => {
   const override: CSSProperties = {
@@ -32,32 +33,35 @@ const Home: NextPage = () => {
     }
   }
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>🦉 Duolingo Stats Card</title>
-        <meta name="description" content="Duolingo stats card for your Github readme page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.container}>
-        <form className={styles.form} onSubmit={e => handleSubmit(e)}>
-          <label htmlFor="username">🧑🏽 Username</label>
-          <input id="username" name="username" type="text"></input>
-          <label htmlFor="theme">🎨 Theme</label>
-          <select name='theme'>
-            {themes.map((theme, index) => {
-              return <option key={index} value={theme}>{theme}</option>
-            })}
-          </select>
-          <label htmlFor="stats">⚙️ Stats</label>
-          <select name='stats'>
-            <option value="crowns">Crowns</option>
-            <option value="xp">XP</option>
-          </select>
-          <button className={styles.btn} type="submit">Submit</button>
-        </form>
-        <p>Check out the <a href="https://github.com/KevzPeter/Duolingo-Stats-Card">Github Page</a> for more info!</p>
-      </main>
-    </div>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>🦉 Duolingo Stats Card</title>
+          <meta name="description" content="Duolingo stats card for your Github readme page" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.container}>
+          <form className={styles.form} onSubmit={e => handleSubmit(e)}>
+            <label htmlFor="username">🧑🏽 Username</label>
+            <input id="username" name="username" type="text"></input>
+            <label htmlFor="theme">🎨 Theme</label>
+            <select name='theme'>
+              {themes.map((theme, index) => {
+                return <option key={index} value={theme}>{theme}</option>
+              })}
+            </select>
+            <label htmlFor="stats">⚙️ Stats</label>
+            <select name='stats'>
+              <option value="xp">XP</option>
+              <option value="crowns">Crowns</option>
+            </select>
+            <button className={styles.btn} type="submit">Submit</button>
+          </form>
+          <p>Check out the <a href="https://github.com/KevzPeter/Duolingo-Stats-Card">Github Page</a> for more info!</p>
+        </main>
+      </div>
+      <Analytics />
+    </>
   )
 }
 
