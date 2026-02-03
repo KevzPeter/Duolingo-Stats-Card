e7eb4024163db853108c71f5204048d6d0b00d5c
KevzPeter <ultimatekevin1@gmail.com>
2026-02-03 11:04:10 -0500

    reset number formatting

--- components/SvgWidget.tsx
+++ components/SvgWidget.tsx
@@ -20,8 +20,8 @@ function formatCreationDate(timestamp: number): string {
 function CalendarIcon(): JSX.Element {
     return (
         <svg width={28} height={28} viewBox="0 0 36 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.87891 10.1518C5.87891 8.81293 6.9643 7.72754 8.3032 7.72754H27.6976C29.0365 7.72754 30.1218 8.81293 30.1218 10.1518V28.5765C30.1218 29.9154 29.0365 31.0008 27.6976 31.0008H8.3032C6.9643 31.0008 5.87891 29.9154 5.87891 28.5765V10.1518Z" fill="beige"/>
-            <path d="M5.87891 9.66698C5.87891 8.59585 6.74722 7.72754 7.81834 7.72754H28.1824C29.2535 7.72754 30.1218 8.59585 30.1218 9.66697V12.5761H5.87891V9.66698Z" fill="#F34848"/>
+            <path d="M5.87891 10.1518C5.87891 8.81293 6.9643 7.72754 8.3032 7.72754H27.6976C29.0365 7.72754 30.1218 8.81293 30.1218 10.1518V28.5765C30.1218 29.9154 29.0365 31.0008 27.6976 31.0008H8.3032C6.9643 31.0008 5.87891 29.9154 5.87891 28.5765V10.1518Z" fill="beige" />
+            <path d="M5.87891 9.66698C5.87891 8.59585 6.74722 7.72754 7.81834 7.72754H28.1824C29.2535 7.72754 30.1218 8.59585 30.1218 9.66697V12.5761H5.87891V9.66698Z" fill="#F34848" />
         </svg>
     );
 }
@@ -62,24 +62,24 @@ export default function SvgWidget({ response, theme, options = { showAvatar: tru
                     <div className="content">
                         <div className="header">
                             {options.showAvatar && avatarBase64 && (
-                                <img 
-                                    className="avatar" 
-                                    src={avatarBase64} 
+                                <img
+                                    className="avatar"
+                                    src={avatarBase64}
                                     alt="User avatar"
                                 />
                             )}
                             <div className="stats">
                                 <span id="streak">
                                     <Streak height={28} width={28} viewBox="0 0 250 283" />
-                                    {numberFormatter(response.streak)} Day streak
+                                    {response.streak} Day streak
                                 </span>
                                 <span id="xp">
                                     <Xp width={28} height={28} viewBox="0 0 56 56" />
-                                    {numberFormatter(response.totalXp)} XP
+                                    {response.totalXp} XP
                                 </span>
                                 {options.showJoined && creationDate && (
                                     <span id="joined">
-                                        <CalendarIcon />
+                                        <CalendarIcon width={28} height={28} />
                                         Joined {creationDate}
                                     </span>
                                 )}
